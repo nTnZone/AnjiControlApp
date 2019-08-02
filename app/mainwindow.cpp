a7b8971cd735846c6607e2556871256dd683370b
“nTnZone <995649319@qq.com>
2019-08-02 23:25:45 +0800

    add a new class pointxy,realtime track the boat on the map

--- app/mainwindow.cpp
+++ app/mainwindow.cpp
@@ -1,27 +1,62 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <qdebug.h>
+#include <iostream>
+
+//这个方法供js调用，发送一个““的信号
+void PointXY::saveMapPoint(QString longti, QString lati)
+{
+    //emit recvdMsg(msg);
+    qDebug() << longti <<','<<lati << endl ;
+    map_longtitude.push_back(longti.toDouble());
+    map_latitude.push_back(lati.toDouble());
+
+}
+
+void PointXY::setMapCenter(double data1,double data2)
+{
+
+    emit mapCenterChanged(QString::number(data1, 10, accuracy),QString::number(data2, 10, accuracy));
+}
+
+
+
+void PointXY::showMapPoint()
+{
+    //qDebug()显示数据会截断，只显示6位数字
+    //qDebug("%.10lf", 445.89099876);
+    std::vector<double>::iterator it;
+    qDebug()<<"longtitude:";
+    for(it=map_longtitude.begin();it!=map_longtitude.end();it++)
+    {
+        qDebug()<<*it;
+    }
+    qDebug()<<"\nlatitude:";
+    for(it=map_latitude.begin();it!=map_latitude.end();it++)
+    {
+        qDebug()<<*it;
+    }
+}
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)//, map_view(new QWebEngineView(this))
 {
     ui->setupUi(this);
 
+    //channel = new QWebChannel();//QwebChannel
+    //channel->registerObject(QString("webobj"),this);
+    //ui->webView->page()->setWebChannel(channel);//将webchannel连接到webview
 
-    //ui->webView->load(QUrl(QDir::currentPath() + "/gaode.html"));
-    //qDebug()<<QDir::currentPath() + "gaode.html";
-
-
-
-    //ui->webView->show();
-
-    channel = new QWebChannel();//QwebChannel
-    channel->registerObject(QString("webobj"),this);
-    ui->webView->page()->setWebChannel(channel);//将webchannel连接到webview
-    ui->webView->load(QUrl(QDir::currentPath() + "/gaode.html"));//加载地图
-    ui->webView->show();
 //    ui->webView->page()->runJavaScript("clearAll()");//QT调用JS
 
+    QWebChannel *webchannel=new QWebChannel();
+
+    webchannel->registerObject("pointxy", pointxy);//注册对象
+    ui->webView->page()->setWebChannel(webchannel);
+
+    ui->webView->load(QUrl(QDir::currentPath() + "/gaode.html"));//加载地图
+    ui->webView->show();
 
 }
 
@@ -35,4 +70,25 @@ void MainWindow::getCoordinates(QString lon,QString lat)
 {
     qDebug() << lon <<','<<lat << endl ;
 //    ui->webView->page()->runJavaScript("clearAll()");
+}
+
+void MainWindow::on_showDataButton_clicked()
+{
+    this->pointxy->showMapPoint();
+}
+
+void MainWindow::on_autoButton_clicked()
+{
+    QTime time;
+    double longi[5]={108.959096,108.973344,108.970082,108.953775,108.918241};
+    double lati[5]={34.25855,34.287205,34.304506,34.322371,34.348595};
+    for(int i=0;i<5;i++)
+    {
+
+
+        time.start();
+        while(time.elapsed() < 5000)             //等待时间流逝5秒钟
+            QCoreApplication::processEvents();   //处理事件
+        this->pointxy->setMapCenter(longi[i],lati[i]);
+    }
 }
--- app/mainwindow.h
+++ app/mainwindow.h
@@ -1,13 +1,43 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
-
+#include <vector>
 #include <QMainWindow>
 #include <QtWebEngineWidgets>
 #include <QWebEngineView> // HTML页面
-#include <QWebChannel>          // C++和JS/HTML双向通信，代替了已淘汰的QtWebFrame的功能
+#include <QWebChannel>    // C++和JS/HTML双向通信，代替了已淘汰的QtWebFrame的功能
+#define size 100
+#define accuracy 6
 namespace Ui {
 class MainWindow;
 }
+
+//WebClass 供JS调用的类，c与js沟通交互的基础
+//PointXY存储船的坐标经纬度
+class  PointXY : public QObject
+{
+    Q_OBJECT
+//public:
+//    explicit PointXY(QObject *parent = nullptr);
+public:
+    //boat_存储卫星定位的经纬度坐标
+    float boat_longtitude;//之后可能为了实际需要修改为vector存储,动态存储
+    float boat_latitude;
+    //map_存储来自地图传来的经纬度坐标
+    std::vector<double> map_longtitude;
+    std::vector<double> map_latitude;
+public:
+    //向网页发送消息
+    void sendMsg(QWebEnginePage* page,const QString& msg);
+    //显示地图传来的坐标点
+    void showMapPoint();
+
+signals:
+    void mapCenterChanged(QString longi,QString lati);
+public slots:
+    //接收网页发来的
+    void saveMapPoint(QString longti, QString lati);
+    void setMapCenter(double data1,double data2);
+};
 
 class MainWindow : public QMainWindow
 {
@@ -17,8 +47,15 @@ public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+public:
+    PointXY *pointxy=new PointXY();
 public slots:
     void getCoordinates(QString lon,QString lat);
+
+private slots:
+    void on_showDataButton_clicked();
+
+    void on_autoButton_clicked();
 
 private:
     Ui::MainWindow *ui;
@@ -27,5 +64,8 @@ private:
 
 //    QWebEngineView *map_view;
 };
+
+
+
 
 #endif // MAINWINDOW_H
